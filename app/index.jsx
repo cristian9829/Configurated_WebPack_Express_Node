1f0ad50dcc3a371d8facf2abf8c8a0411ba01d89
cristian <cristian982940@gmail.com>
2019-06-12 16:40:22 -0500

    init configurated

---
+++ app/index.jsx
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+const App = () => <h1>HOla react :) </h1>
+
+render( <App/>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './app/index.html',
+  filename: "index.html",
+  inject: "body"
+})
+
+const config = {
+  entry: './app/index.jsx',
+  output: {
+    path: path.resolve("dist"),
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules : [
+      {
+        test: /.jsx$/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
+
+module.exports = config;
